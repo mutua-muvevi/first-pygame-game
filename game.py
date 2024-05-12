0948a266ae27b72b32a2f1c5a4fc124f9bf530f5
mutua-muvevi <samstore637@gmail.com>
2024-05-12 06:00:35 +0300

    Added main player

--- game.py
+++ game.py
@@ -2,6 +2,9 @@ import sys
 
 import pygame
 
+from scripts.entities import PhysicsEntity
+from scripts.utils import load_image
+
 
 class Game:
 	def __init__(self):
@@ -10,30 +13,25 @@ class Game:
 		pygame.display.set_caption("The Ninja Game")
 
 		self.screen = pygame.display.set_mode((640, 480))
-		self.clock = pygame.time.Clock()
+		self.display = pygame.Surface((320, 320))
 
-		self.img = pygame.image.load("data/images/clouds/cloud_1.png")
-		self.img.set_colorkey((0, 0, 0))
-		self.img_pos = [160, 260]
+		self.clock = pygame.time.Clock()
 
 		self.movement = [False, False]
 
-		self.collision_area = pygame.Rect(50, 50, 300, 50)
+		self.assets = {
+			"player": load_image("entities/player.png")
+		}
+
+		self.player = PhysicsEntity(self, "player", (50, 50), (8, 15))
 
 	def run(self):
 
 		while True:
-			self.screen.fill((14, 219, 248))
+			self.display.fill((14, 219, 248))
 
-			img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
-
-			if img_r.colliderect(self.collision_area):
-				pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
-			else:
-				pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
-
-			self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5  # multiply increases the speed
-			self.screen.blit(self.img, self.img_pos)  # loads the image to screen using blit
+			self.player.update((self.movement[1] - self.movement[0], 0))
+			self.player.render(self.display)
 
 			# gets events to ensure that there is input if it is not written the cursor will be loading
 			for event in pygame.event.get():
@@ -42,20 +40,20 @@ class Game:
 					sys.exit()
 
 				if event.type == pygame.KEYDOWN:  # if a given key is pressed
-					if event.key == pygame.K_UP:
+					if event.key == pygame.K_LEFT:
 						self.movement[0] = True
 
-					if event.key == pygame.K_DOWN:
+					if event.key == pygame.K_RIGHT:
 						self.movement[1] = True
 
 				if event.type == pygame.KEYUP:  # if a given key is released
-					if event.key == pygame.K_UP:
+					if event.key == pygame.K_LEFT:
 						self.movement[0] = False
 
-					if event.key == pygame.K_DOWN:
+					if event.key == pygame.K_RIGHT:
 						self.movement[1] = False
 
-
+			self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
 
 			pygame.display.update()
 			self.clock.tick(60)
