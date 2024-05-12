e69a92f7c9c4f86c69ac1c574c19fcc5f20a5ccf
mutua-muvevi <samstore637@gmail.com>
2024-05-12 06:58:23 +0300

    Added main player

--- game.py
+++ game.py
@@ -3,7 +3,8 @@ import sys
 import pygame
 
 from scripts.entities import PhysicsEntity
-from scripts.utils import load_image
+from scripts.utils import load_image, load_images
+from scripts.tilemap import Tilemap
 
 
 class Game:
@@ -20,15 +21,21 @@ class Game:
 		self.movement = [False, False]
 
 		self.assets = {
-			"player": load_image("entities/player.png")
+			"decor": load_images("tiles/decor"),
+			"grass": load_images("tiles/grass"),
+			"large_decor": load_images("tiles/large_decor"),
+			"stone": load_images("tiles/stone"),
+			"player": load_image("entities/player.png"),
 		}
 
 		self.player = PhysicsEntity(self, "player", (50, 50), (8, 15))
+		self.tilemap = Tilemap(self, tile_size=16)
 
 	def run(self):
 
 		while True:
 			self.display.fill((14, 219, 248))
+			self.tilemap.render(self.display)
 
 			self.player.update((self.movement[1] - self.movement[0], 0))
 			self.player.render(self.display)
