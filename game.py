5ec59664f49e0b71d1562d8663580be6bdfbc82f
mutua-muvevi <samstore637@gmail.com>
2024-05-14 00:49:26 +0300

    added clouds, camera, sky and paralax effect for the clouds

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ import pygame
 from scripts.entities import PhysicsEntity
 from scripts.utils import load_image, load_images
 from scripts.tilemap import Tilemap
+from scripts.clouds import Clouds
 
 
 class Game:
@@ -14,7 +15,7 @@ class Game:
         pygame.display.set_caption("The Ninja Game")
 
         self.screen = pygame.display.set_mode((640, 480))
-        self.display = pygame.Surface((320, 320))
+        self.display = pygame.Surface((320, 240))
         self.clock = pygame.time.Clock()
         self.movement = [False, False]
         self.assets = {
@@ -23,21 +24,31 @@ class Game:
             "large_decor": load_images("tiles/large_decor"),
             "stone": load_images("tiles/stone"),
             "player": load_image("entities/player.png"),
+            "background": load_image("background.png"),
+            "clouds": load_images("clouds")
         }
+
+        self.clouds = Clouds(self.assets["clouds"], count=16)
         self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
         self.tilemap = Tilemap(self, tile_size=16)
+        self.scroll = [0, 0]
 
     def run(self):
 
         while True:
-            self.display.fill((14, 219, 248))
-            self.tilemap.render(self.display)
+            self.display.blit(self.assets['background'], (0, 0))
+
+            self.scroll[0] += (self.player.rect().centerx - self.display.get_width() / 2 - self.scroll[0]) / 30
+            self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 30
+            render_scroll = (int(self.scroll[0]), int(self.scroll[1]))
+
+            self.clouds.update()
+            self.clouds.render(self.display, offset=render_scroll)
+
+            self.tilemap.render(self.display, offset=render_scroll)
 
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-            self.player.render(self.display)
-
-            # print(self.tilemap.tiles_around(self.player.pos))
-            # print(self.tilemap.physics_rects_around(self.player.pos))
+            self.player.render(self.display, offset=render_scroll)
 
             # gets events to ensure that there is input if it is not written the cursor will be loading
             for event in pygame.event.get():
