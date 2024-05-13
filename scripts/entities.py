a50af97b62377ea03ff0102987fe9896c4176af7
mutua-muvevi <samstore637@gmail.com>
2024-05-14 02:29:52 +0300

    added animation for the player

--- scripts/entities.py
+++ scripts/entities.py
@@ -14,6 +14,10 @@ class PhysicsEntity:
             "left": False,
             "right": False,
         }
+        self.action = ""
+        self.anim_offset = (-3, -3)
+        self.flip = False
+        self.set_action("idle")
 
     def rect(self):
         return pygame.Rect(
@@ -22,6 +26,11 @@ class PhysicsEntity:
             self.size[0],
             self.size[1]
         )
+
+    def set_action(self, action):
+        if action != self.action:
+            self.action = action
+            self.animation = self.game.assets[self.type + "/" + self.action].copy()
 
     def update(self, tilemap, movement=(0, 0)):
         self.collisions = {
@@ -50,6 +59,11 @@ class PhysicsEntity:
 
                 self.pos[0] = entity_rect.x
 
+        if movement[0] > 0:
+            self.flip = False
+        if movement[0] < 0:
+            self.flip = True
+
         self.pos[1] += frame_movement[1]
         entity_rect = self.rect()
 
@@ -68,11 +82,31 @@ class PhysicsEntity:
         if self.collisions["down"] or self.collisions["up"]:
             self.velocity[1] = 0
 
+        self.animation.update()
+
     def render(self, surf, offset=(0, 0)):
         surf.blit(
-            self.game.assets["player"],
-            (
-                self.pos[0] - offset[0],
-                self.pos[1] - offset[1]
-            )
+            pygame.transform.flip(self.animation.img(), self.flip, False),
+            (self.pos[0] - offset[0] + self.anim_offset[0], self.pos[1] - offset[1] + self.anim_offset[1])
         )
+
+
+class Player(PhysicsEntity):
+    def __init__(self, game, pos, size):
+        super().__init__(game, "player", pos, size)
+        self.air_time = 0
+
+    def update(self, tilemap, movement=(0, 0)):
+        super().update(tilemap, movement=movement)
+
+        self.air_time += 1
+
+        if self.collisions["down"]:
+            self.air_time = 0
+
+        if self.air_time > 4:
+            self.set_action("jump")
+        elif movement[0] != 0:
+            self.set_action("run")
+        else:
+            self.set_action("idle")
